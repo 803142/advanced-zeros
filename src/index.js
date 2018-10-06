8a4698702aa60cf5aafac9fe92404ab5a934e892
803142 <803142@gmail.com>
2018-10-06 15:59:30 +0300

    the job is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+const counter=(lastP,lastMn,num)=>{     //COUNTING zeros   
+        let count=0;
+        while (num>0){
+        num=Math.floor(num/lastMn);        
+        count=count+num;
+      }
+      console.log({lastMn,lastP,count});
+    return Math.floor(count/lastP);
+  }
+let lastP  = 1;                       //
+let lastMn = base;
+let num    = number; 
+
+let count=counter(lastP,lastMn,num);
+
+let midle  = 0;
+let i      = 2;
+while (i <= (base/i)) {                   //looking for multipliers
+    lastP = 0;
+      while (lastMn%i==0 && i!=lastMn){   //multiplier degree
+      lastMn=lastMn/i;
+      lastP++;
+      }
+    if (lastP!=0)  {                      //if found
+      if(lastMn==i){
+        lastP++;
+        midle = counter(lastP,lastMn,num);
+      }
+      else{
+        midle=Math.min(counter(1,lastMn,num),counter(lastP,i,num))
+      }
+
+      if (midle>count)count=midle;         
+      }       
+    i++;
+} 
+  return count;
 }
