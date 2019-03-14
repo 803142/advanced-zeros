518ea6c44c05fe266044f5ea66ac3e60a683cb80
803142 <803142@gmail.com>
2019-03-14 19:33:36 +0300

    fix: chenged names

--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
 module.exports = function getZerosCount(number, base) {
-const counter=(lastP,lastMn,num)=>{    
-        let count=0;
-        while (num>0){
-        num=Math.floor(num/lastMn);        
-        count=count+num;
+const counter = (lastP,lastMultipl,num) => {    
+        let count = 0;
+        while (num > 0){
+          num = Math.floor(num/lastMultipl);        
+          count = count+num;
         }
       return Math.floor(count/lastP);
       }
 let lastP = 1;                       
-let lastMn = base;
+let lastMultipl = base;
 let num = number; 
 
-let count = counter(lastP,lastMn,num);
+let count = counter(lastP,lastMultipl,num);
 
 let midle = 0;
 let i = 2;
 while (i <= (base/i)) {                   
   lastP = 0;
-    while (lastMn%i == 0 && i != lastMn){  
-      lastMn = lastMn/i;
+    while (lastMultipl%i == 0 && i != lastMultipl){  
+      lastMultipl = lastMultipl/i;
       lastP++;
     }
     if (lastP != 0)  {                      
-      if (lastMn == i) {
+      if (lastMultipl == i) {
         lastP++;
-        midle = counter(lastP,lastMn,num);
+        midle = counter(lastP,lastMultipl,num);
       }else {
-        midle=Math.min(counter(1,lastMn,num),counter(lastP,i,num))
+        midle=Math.min(counter(1,lastMultipl,num),counter(lastP,i,num))
       }
     if (midle>count)count=midle;         
     }       
