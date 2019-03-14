c27592e73a8ef1594d2eacbf31328d012d4ef25a
803142 <803142@gmail.com>
2019-03-14 19:29:52 +0300

    style: deleted coments

--- src/index.js
+++ src/index.js
@@ -1,38 +1,36 @@
 module.exports = function getZerosCount(number, base) {
-const counter=(lastP,lastMn,num)=>{     //COUNTING zeros   
+const counter=(lastP,lastMn,num)=>{    
         let count=0;
         while (num>0){
         num=Math.floor(num/lastMn);        
         count=count+num;
-      }
+        }
       return Math.floor(count/lastP);
       }
-let lastP  = 1;                       //
+let lastP = 1;                       
 let lastMn = base;
-let num    = number; 
+let num = number; 
 
-let count=counter(lastP,lastMn,num);
+let count = counter(lastP,lastMn,num);
 
-let midle  = 0;
-let i      = 2;
-while (i <= (base/i)) {                   //looking for multipliers
-    lastP = 0;
-      while (lastMn%i==0 && i!=lastMn){   //multiplier degree
-      lastMn=lastMn/i;
+let midle = 0;
+let i = 2;
+while (i <= (base/i)) {                   
+  lastP = 0;
+    while (lastMn%i == 0 && i != lastMn){  
+      lastMn = lastMn/i;
       lastP++;
-      }
-    if (lastP!=0)  {                      //if found
-      if(lastMn==i){
+    }
+    if (lastP != 0)  {                      
+      if (lastMn == i) {
         lastP++;
         midle = counter(lastP,lastMn,num);
-      }
-      else{
+      }else {
         midle=Math.min(counter(1,lastMn,num),counter(lastP,i,num))
       }
-
-      if (midle>count)count=midle;         
-      }       
-i++;
+    if (midle>count)count=midle;         
+    }       
+  i++;
 } 
-  return count;
+return count;
 }
